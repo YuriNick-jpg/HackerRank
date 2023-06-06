ce658ab39663ad66469866715436bf16649d3129
Nicolas Yuri <79637247+YuriNick-jpg@users.noreply.github.com>
2023-06-06 11:09:17 -0300

    Delete simple-array-sum.js

--- simple-array-sum.js
+++
@@ -1,9 +0,0 @@
-function simpleArraySum(ar) {
-    let soma = 0
-    let i = 0
-    for (let i = 0; i < ar.length; i++) {
-        const element = ar[i];
-        soma = soma + element
-    }
-    return [soma];
-}
