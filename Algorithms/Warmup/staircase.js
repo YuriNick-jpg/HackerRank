0d9c1cbb4b73fb078d10abe098d533a7969baf8f
Nicolas Yuri <79637247+YuriNick-jpg@users.noreply.github.com>
2023-06-07 13:42:18 -0300

    Create staircase.js

---
+++ Algorithms/Warmup/staircase.js
@@ -0,0 +1,7 @@
+function staircase(n) {
+    for (let i = 0; i < n; i++) {
+        let space = " ".repeat(n - i - 1);
+        let hash = "#".repeat(i + 1);
+        console.log(space + hash);
+      }
+}
